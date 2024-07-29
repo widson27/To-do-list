61f9826fe4accad643cad9e7ae3135963aa5f6c8
Widson Martins <131882974+widson27@users.noreply.github.com>
2024-07-29 15:18:54 -0300

    implementando typescript

---
+++ src/main.ts
@@ -0,0 +1,2 @@
+import "./components/nova-tarefa-component.js";
+import "./components/filtar-tarefas-component.js";
---
+++ src/types/TipoTarefa.ts
@@ -0,0 +1,5 @@
+export interface Tarefa {
+    descricao: string;
+    concluida: boolean;
+}
+
